505835edee0784d704f82e7529d6c9e4199819d4
EDS <septempo@yandex.ru>
2025-01-25 17:09:27 +0300

    25-01-25 on the work

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React from "react"
 export const App = () => {
-  // return <h1 className="my-class">
-  //   Hello <br /> Rim!
-  // </h1>
-
-  return React.createElement(
-    'h1',
-    {className: 'my-class' },
-    'Hello',
-    React.createElement('br'),
-    'Rim!'
+  const ideas = []
+  return (
+    <div>
+      <h1>Ideanick</h1>
+      <div>
+        <div>
+          <h2>Idea 1</h2>
+          <p>Idea 1 description...</p>
+        </div>
+        <div>
+          <h2>Idea 2</h2>
+          <p>Idea 2 description...</p>
+        </div>
+        <div>
+          <h2>Idea 3</h2>
+          <p>Idea 3 description...</p>
+        </div>
+      </div>
+    </div>
   )
 }
